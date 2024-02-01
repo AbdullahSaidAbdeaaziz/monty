dbd4d19f673d2444c230ba3c87cd0a1de9e71b69
AbdullahSaidAbdeaziz <gitHubjoe2@gmail.com>
2024-02-01 19:52:42 +0200

    Tasks Done

--- main.c
+++ main.c
@@ -1,11 +1,84 @@
-#include "main.h"
+#include "monty.h"
+
 
 /**
  * main - entry point of program
+ * @argc: arguments count
+ * @argv: list of arguments
  * Return: 0 on success
-*/
+ */
 
-int main(void)
+int main(int argc, char *argv[])
 {
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	open_file(argv[1]);
+	free_nodes();
 	return (0);
+}
+
+/**
+ * create_node - Creates a node.
+ * @n: Number to go inside the node.
+ * Return: Upon sucess a pointer to the node. Otherwise NULL.
+ */
+stack_t *create_node(int n)
+{
+	stack_t *node;
+
+	node = malloc(sizeof(stack_t));
+	if (node == NULL)
+		err_handle(4);
+	node->next = NULL;
+	node->prev = NULL;
+	node->n = n;
+	return (node);
+}
+
+/**
+ * free_nodes - Frees nodes in the stack.
+ * Return: None
+ */
+void free_nodes(void)
+{
+	stack_t *tmp;
+
+	if (head == NULL)
+		return;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
+
+
+/**
+ * enqueue - add node to queue.
+ * @new_node: Node that will be added.
+ * @ln: line number of opcode.
+ * Return: None
+*/
+void enqueue(stack_t **new_node, __attribute__((unused))unsigned int ln)
+{
+	stack_t *tmp;
+
+	if (new_node == NULL || *new_node == NULL)
+		exit(EXIT_FAILURE);
+	if (head == NULL)
+	{
+		head = *new_node;
+		return;
+	}
+	tmp = head;
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+
+	tmp->next = *new_node;
+	(*new_node)->prev = tmp;
 }
--- monty.h
+++ monty.h
@@ -1,9 +1,11 @@
 #ifndef MONTY_H
 #define MONTY_H
-#include <stdlib.h>
 #include <stdio.h>
-#include <stdarg.h>
+#include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdarg.h>
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -34,5 +36,48 @@ typedef struct instruction_s
 	char *opcode;
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+
+extern stack_t *head = NULL;
+typedef void (*op_func)(stack_t **, unsigned int);
+
+/* file operations*/
+void open_file(char *file_name);
+int parse_line(char *buffer, int line_number, int format);
+void read_file(FILE *file);
+int len_chars(FILE *file);
+void find_func(char *, char *, int, int);
+
+/*Stack operations*/
+stack_t *create_node(int n);
+void free_nodes(void);
+void display_stack(stack_t **, unsigned int);
+void push(stack_t **, unsigned int);
+void enqeue(stack_t **, unsigned int);
+
+void call_func(op_func, char *, char *, int, int);
+
+void top(stack_t **, unsigned int);
+void pop(stack_t **, unsigned int);
+void nop(stack_t **, unsigned int);
+void swap(stack_t **, unsigned int);
+
+/*Math operations with nodes*/
+void add(stack_t **, unsigned int);
+void sub(stack_t **, unsigned int);
+void div(stack_t **, unsigned int);
+void mul(stack_t **, unsigned int);
+void mod(stack_t **, unsigned int);
+
+/*String operations*/
+void display_char(stack_t **, unsigned int);
+void display_str(stack_t **, unsigned int);
+void rotl(stack_t **, unsigned int);
+
+/*Error hanlding*/
+void err_handle(int error_code, ...);
+void more_err(int error_code, ...);
+void string_err(int error_code, ...);
+void rotr(stack_t **, unsigned int);
+
 
 #endif
